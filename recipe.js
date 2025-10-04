7dad681ffd423e201d042421a1d9fab0a3c09148
adityaatre26 <aditya.atre26@gmail.com>
2025-10-04 12:22:25 +0530

    Added everything

--- recipe.js
+++ recipe.js
@@ -12,13 +12,38 @@ window.addEventListener("load", async () => {
     const oneRecipe = document.querySelector(".oneRecipe");
     oneRecipe.style.display = "block";
 
-    const tbd = JSON.stringify(res);
-    const para = document.createElement("p");
-    const node = document.createTextNode(res);
-    para.appendChild(node);
+    let ingredients = [];
+    let str = "strIngredient";
 
-    const element = document.querySelector(".recipeIst");
-    element.appendChild(para);
+    for (let i = 1; i <= 20; i++) {
+      let temp = str + i;
+
+      //   console.log(res[0][temp]);
+      if (res[0][temp] && res[0][temp].length > 0) {
+        ingredients.push(res[0][temp]);
+      }
+    }
+    console.log(ingredients);
+
+    let measurements = [];
+    str = "strMeasure";
+
+    for (let i = 1; i <= 20; i++) {
+      let temp = str + i;
+
+      //   console.log(res[0][temp]);
+      if (res[0][temp] && res[0][temp].length > 0) {
+        measurements.push(res[0][temp]);
+      }
+    }
+
+    console.log(measurements);
+
+    const heading = document.querySelector(".recipeName");
+    const instr = document.querySelector(".recipeName");
+
+    heading.innerHTML = res[0].strMeal;
+    instr.innerHTML = res[0].strInstructions;
   } else {
     const catalogue = document.querySelector(".availableRecipes");
     catalogue.style.display = "block";
@@ -27,7 +52,6 @@ window.addEventListener("load", async () => {
     for (let i = 0; i < res.length; i++) {
       let recipeName = res[i].strMeal;
       const divi = document.createElement("div");
-      const innerDivi = document.createElement("div");
       const para = document.createElement("p");
       const linki = document.createElement("a");
       const node = document.createTextNode(recipeName);
@@ -37,6 +61,7 @@ window.addEventListener("load", async () => {
       divi.appendChild(linki);
 
       divi.classList.add("recipeCard");
+      linki.classList.add(recipeName);
 
       catalogue.appendChild(divi);
     }
